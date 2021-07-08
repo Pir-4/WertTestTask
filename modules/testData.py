7c57b7c1149003817911971bb1ad96f42214381f
valentin <valentin-42@mail.ru>
2021-07-08 16:40:57 +0300

    extend test data

--- modules/testData.py
+++ modules/testData.py
@@ -3,6 +3,7 @@ import random
 def positive_test_data():
     """"""
     return [
+            {"lon": 100.75, "lat": 1.5, "date": "2014-02-01", "dim": 1.025},
             {"lon": Longitude(), "lat": Latitude()},
             {"lon": 0, "lat": 0},
             {"lon": 0, "lat": 90},
@@ -14,8 +15,9 @@ def positive_test_data():
             {"lon": 180, "lat": 90},
             {"lon": 180, "lat": -90},
             {"lon":  Longitude(), "lat": Latitude(), "date": "2014-02-01"},
-            {"lon":  Longitude(), "lat": Latitude(), "date": "2014-02-01", "dim": 0.025},
-            {"lon":  Longitude(), "lat": Latitude(), "dim": 0.025}
+            {"lon":  Longitude(), "lat": Latitude(), "date": "2014-02-01", "dim": Dim()},
+            {"lon":  Longitude(), "lat": Latitude(), "dim": Dim()},
+            {"lon": Longitude(), "lat": Latitude(), "date": "2014-02-01", "cloud_score": False},
             ]
 
 def negative_test_data():
@@ -30,10 +32,15 @@ def negative_test_data():
             {"lon": Longitude(), "lat": "badstr", "date": "2014-02-01"},
             {"lon": Longitude(), "lat": Latitude(), "date": "badstr"},
             {"lon": Longitude(), "lat": Latitude(), "date": "2014-02-01", "dim": "badstr"},
+            {"lon": Longitude(), "lat": Latitude(), "date": "2014-02-01", "cloud_score": True},
+            {"lon": 100.75, "lat": 1.5, "date": "2014-02-01", "dim": Dim(83, 100)},
             ]
 
 def Latitude(start=-90, stop=90):
-    return round(random.uniform(start, stop), 6)
+    return round(random.uniform(start, stop+1), 6)
 
 def Longitude(start=-180, stop=180):
-     return round(random.uniform(start, stop), 6)
+     return round(random.uniform(start, stop+1), 6)
+
+def Dim(start=0.00000000001, stop=82):
+        return random.uniform(start, stop+1)
--- tests/test_rest_earth.py
+++ tests/test_rest_earth.py
@@ -1,30 +1,37 @@
 import pytest
 import os
+from http import HTTPStatus
 
 from modules import nasa_restapi, testData
 
 Nasa_Api = None
 
 @pytest.fixture(scope="module", autouse=True)
-def initialized_nasa_rstapi():
-    """Connect to db before testing, disconnect after."""
+def initialized_nasa_rest_obj():
+    """initialize nasa api class"""
     global Nasa_Api
     Nasa_Api = nasa_restapi.NasaRestApi(os.environ["ApiKey"])
     yield
 
 @pytest.mark.parametrize('params', testData.positive_test_data())
 def test_earth_get_positive(params):
-    """"""
+    """Verify planetary/earth with right parameters"""
     result = Nasa_Api.GetEarthImagery(params)
-    #assert result["success"]
-    assert result["status_code"] == 200
+    assert result["status_code"] == HTTPStatus.OK
     #TODO check the text is image
 
 
 @pytest.mark.parametrize('params', testData.negative_test_data())
 def test_earth_get_negative(params):
-    """"""
+    """Verify planetary/earth with negative parameters"""
     result = Nasa_Api.GetEarthImagery(params)
-    assert not result["success"]
-    assert result["status_code"] != 200
+    assert not result["success"] and result["status_code"] != HTTPStatus.OK
     #TODO check the text is not image
+
+
+@pytest.mark.parametrize('params', testData.positive_test_data())
+def test_earth_get_without_apiKey(params):
+    """Verify planetary/earth request without api key"""
+    nasa_api = nasa_restapi.NasaRestApi(api_key=None)
+    result = nasa_api.GetEarthImagery(params)
+    assert not result["success"] and result["status_code"] == HTTPStatus.FORBIDDEN
