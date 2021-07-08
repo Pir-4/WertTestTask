b5577a76575a015905862c3947b1ace316f59adf
valentin <valentin-42@mail.ru>
2021-07-08 13:05:05 +0300

    check NASA class

--- tests/test_rest_earth.py
+++ tests/test_rest_earth.py
@@ -1,5 +1,14 @@
 import pytest
+import os
+
+from modules import nasa_restapi
+
+ApiKey = os.environ["ApiKey"]
 
 def test_earth_get_positive():
     """"""
+    nasa_api = nasa_restapi.NasaRestApi(ApiKey)
+    #result = nasa_api.GetEarthImagery({"lon": 100.75,"lat": 1.5, "date": "2014-02-01"})
+    result = nasa_api.GetEarthImagery()
+
     assert True
