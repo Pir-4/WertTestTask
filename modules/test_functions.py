87604a957f8410fb350a8a06ceedff15dae5ba7b
valentin <valentin-42@mail.ru>
2021-07-09 13:12:07 +0300

    try to save in the file

--- modules/test_functions.py
+++ modules/test_functions.py
@@ -1,9 +1,14 @@
 import io
 from PIL import Image
+import tempfile
 
 def is_Image(inputStr):
     """"""
     #PIL.UnidentifiedImageError: cannot identify image file <_io.BytesIO object at 0x000001B58E225A40>
-    t = io.BytesIO(str.encode(inputStr))
-    t.seek(0)
-    return Image.open(t)
+    #t = io.BytesIO(str.encode(inputStr))
+    #t.seek(0)
+    #return Image.open(t)
+    with tempfile.NamedTemporaryFile() as temp:
+        temp.write(str.encode(inputStr))
+        temp.flush()
+        im = Image.open(temp)
